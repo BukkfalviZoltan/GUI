bf845ec516577b825a6e75b8fcdd32dc2d997861
BukkfalviZoltan <bukkfalviz@gmail.com>
2020-12-12 19:54:32 +0100

    Létrehoztam egy .py és egy .tx fájlt

---
+++ Comb.py
@@ -0,0 +1,23 @@
+#A GUI-hoz PySimpleGui-t használtam. Ennek telepíéséhez az alábbit kell a Terminal ablakban beírni és futtatni: python -m pip install pysimplegui
+
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Üdvözlünk a comb adagolóban!")], [sg.Button("Libacomb adagoló"), sg.Button("Kacsacomb adagoló"), sg.Button("Csirkecomb adagoló")], [sg.Button("Jóllaktam")]]
+
+# Ablak létrehozása
+window = sg.Window("COMBADAGOLO", layout)
+
+# Event loop\eseménykezelő ciklus létrehozása
+while True:
+    event, values = window.read()
+    #Program vége, ha a felhasználó a "Jóllaktam" gombra kattint, illetve, ha az ablakbezáró gombot használja
+    if event == "Jóllaktam" or event == sg.WIN_CLOSED:
+        sg.popup('Viszontlátásra!')
+        break
+    elif event == "Kacsacomb adagoló":
+        sg.popup('Kapsz egy kacsacombot!')
+    elif event == "Libacomb adagoló":
+        sg.popup('Kapsz egy libacombot!')
+    elif event == "Csirkecomb adagoló":
+        sg.popup('Kapsz egy csirkecombot!')    
+window.close()
